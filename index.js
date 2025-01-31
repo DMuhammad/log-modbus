b7fc1a660b7cde4699314379d1962c9a6afb80f7
Dzikri Muhammad <notfnd123@gmail.com>
2025-01-31 16:57:20 +0700

    update: fix get data from tk4s

--- index.js
+++ index.js
@@ -1,44 +1,54 @@
+const axios = require("axios");
 const delay = require("delay");
-const ModbusRTU = require("modbus-serial");
-// const modbus = require("jsmodbus");
+const FormData = require("form-data");
+const modbus = require("jsmodbus");
 const { SerialPort } = require("serialport");
 require("dotenv").config();
 
-// console.log(SerialPort.list());
-
 const configuration = new SerialPort({
-  path: process.env.PATH,
+  path: "COM3",
   baudRate: 9600,
-  // parity: "even",
-  // stopBits: 1,
-  // dataBits: 8,
+  parity: "even",
+  stopBits: 1,
+  dataBits: 8,
 });
-console.log(configuration);
+const client = new modbus.client.RTU(configuration, 1, 2000);
+
+const scheduleNextExecution = (callback) => {
+  const delay = 30 * 60 * 1000;
+  setTimeout(callback, delay);
+};
+
+const fetchData = async () => {
+  const startPolling = async () => {
+    try {
+      const res = await client.readInputRegisters(1000, 1);
+      const data = res.response.body.values[0];
+      const form = new FormData();
+      form.append("area", "mjl");
+      form.append("nilai", data);
+
+      const response = await axios.post(process.env.server_url, form, {
+        headers: {
+          ...form.getHeaders(),
+        },
+      });
+
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+
+    scheduleNextExecution(() => startPolling());
+  };
+  return startPolling();
+};
 
 const main = async () => {
-  const client = new ModbusRTU();
-  // const client = new modbus.client.RTU(configuration, 1, 2000);
-  // console.log(client);
-
-  // const numberOfRegisters = 2;
-  client.connectRTUBuffered("COM7", { baudRate: 9600 });
-  await client.setID(1);
-
-  client.readHoldingRegisters(0, 10, function (err, data) {
-    console.log(data.data);
+  configuration.on("open", async () => {
+    console.log("Serial port opened");
+    await fetchData();
   });
-
-  // const data = await client.readInputRegisters(1000, 1);
-  // // const data = await client.readInputRegisters(1001, 10);
-  // console.log(data);
-
-  // setTimeout(
-  //   () =>
-  //     main().catch((error) => {
-  //       console.log(error);
-  //     }),
-  //   1000
-  // );
 };
 
 main().catch((error) => {
