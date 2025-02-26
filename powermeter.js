05b363df83b5e2d0f41d3058ca85e6f702f314ae
Dzikri Muhammad <notfnd123@gmail.com>
2025-02-26 13:21:39 +0700

    update add addressing for cos p, thd a, and thd v

--- powermeter.js
+++ powermeter.js
@@ -13,7 +13,7 @@ const log = (color, message) => {
 };
 
 const configuration = new SerialPort({
-  path: "COM7",
+  path: process.env.serial_port,
   baudRate: 9600,
   parity: "none",
   stopBits: 1,
@@ -45,6 +45,13 @@ const areas = {
   20: "Lighting 3rd Floor",
 };
 
+const combineDWord = (highWord, lowWord) => {
+  highWord = highWord >>> 0;
+  lowWord = lowWord >>> 0;
+
+  return highWord * 0x10000 + lowWord; // 0x10000 == 2^16
+};
+
 const getPowerMeterData = async (slaveId) => {
   try {
     const client = new modbus.client.RTU(configuration, slaveId, 5000);
@@ -56,24 +63,45 @@ const getPowerMeterData = async (slaveId) => {
     let registerV1;
     let registerV2;
     let registerV3;
+    let registerCosP;
+    let registerThdA1;
+    let registerThdA2;
+    let registerThdA3;
+    let registerThdV1;
+    let registerThdV2;
+    let registerThdV3;
     let registerKwh;
 
     if (cvms.includes(slaveId)) {
-      registerV1 = 0x0000;
-      registerV2 = 0x0010;
-      registerV3 = 0x0020;
-      registerA1 = 0x0002;
-      registerA2 = 0x0012;
-      registerA3 = 0x0022;
-      registerKwh = 0x005e;
+      registerV1 = 0x00;
+      registerV2 = 0x01;
+      registerV3 = 0x02;
+      registerA1 = 0x02;
+      registerA2 = 0x12;
+      registerA3 = 0x22;
+      registerCosP = 0x3a;
+      registerThdV1 = 0x46;
+      registerThdV2 = 0x48;
+      registerThdV3 = 0x4a;
+      registerThdA1 = 0x4c;
+      registerThdA2 = 0x4e;
+      registerThdA3 = 0x50;
+      registerKwh = 0xdc;
     } else if (nrgs.includes(slaveId)) {
-      registerV1 = 0x0000;
-      registerV2 = 0x000a;
-      registerV3 = 0x0014;
-      registerA1 = 0x0002;
-      registerA2 = 0x000c;
-      registerA3 = 0x0016;
-      registerKwh = 0x003c;
+      registerV1 = 0x00;
+      registerV2 = 0x0a;
+      registerV3 = 0x14;
+      registerA1 = 0x02;
+      registerA2 = 0x0c;
+      registerA3 = 0x16;
+      registerCosP = 0x24;
+      registerThdV1 = 0x30;
+      registerThdV2 = 0x32;
+      registerThdV3 = 0x34;
+      registerThdA1 = 0x36;
+      registerThdA2 = 0x38;
+      registerThdA3 = 0x3a;
+      registerKwh = 0x3c;
     }
 
     const dataV1 = await client.readHoldingRegisters(
@@ -100,20 +128,119 @@ const getPowerMeterData = async (slaveId) => {
       registerA3,
       numberOfRegisters
     );
+    const dataCosP = await client.readHoldingRegisters(
+      registerCosP,
+      numberOfRegisters
+    );
+    const dataThdA1 = await client.readHoldingRegisters(
+      registerThdA1,
+      numberOfRegisters
+    );
+    const dataThdA2 = await client.readHoldingRegisters(
+      registerThdA2,
+      numberOfRegisters
+    );
+    const dataThdA3 = await client.readHoldingRegisters(
+      registerThdA3,
+      numberOfRegisters
+    );
+    const dataThdV1 = await client.readHoldingRegisters(
+      registerThdV1,
+      numberOfRegisters
+    );
+    const dataThdV2 = await client.readHoldingRegisters(
+      registerThdV2,
+      numberOfRegisters
+    );
+    const dataThdV3 = await client.readHoldingRegisters(
+      registerThdV3,
+      numberOfRegisters
+    );
     const dataKwh = await client.readHoldingRegisters(
       registerKwh,
       numberOfRegisters
     );
 
+    const a1 =
+      combineDWord(
+        dataA1.response.body.values[0],
+        dataA1.response.body.values[1]
+      ) / 1000;
+    const a2 =
+      combineDWord(
+        dataA2.response.body.values[0],
+        dataA2.response.body.values[1]
+      ) / 1000;
+    const a3 =
+      combineDWord(
+        dataA3.response.body.values[0],
+        dataA3.response.body.values[1]
+      ) / 1000;
+    const v1 =
+      combineDWord(
+        dataV1.response.body.values[0],
+        dataV1.response.body.values[1]
+      ) / 10;
+    const v2 =
+      combineDWord(
+        dataV2.response.body.values[0],
+        dataV2.response.body.values[1]
+      ) / 10;
+    const v3 =
+      combineDWord(
+        dataV3.response.body.values[0],
+        dataV3.response.body.values[1]
+      ) / 10;
+    const thdA1 = combineDWord(
+      dataThdA1.response.body.values[0],
+      dataThdA1.response.body.values[1]
+    );
+    const thdA2 = combineDWord(
+      dataThdA2.response.body.values[0],
+      dataThdA2.response.body.values[1]
+    );
+    const thdA3 = combineDWord(
+      dataThdA3.response.body.values[0],
+      dataThdA3.response.body.values[1]
+    );
+    const thdV1 = combineDWord(
+      dataThdV1.response.body.values[0],
+      dataThdV1.response.body.values[1]
+    );
+    const thdV2 = combineDWord(
+      dataThdV2.response.body.values[0],
+      dataThdV2.response.body.values[1]
+    );
+    const thdV3 = combineDWord(
+      dataThdV3.response.body.values[0],
+      dataThdV3.response.body.values[1]
+    );
+    const cosP = combineDWord(
+      dataCosP.response.body.values[0],
+      dataCosP.response.body.values[1]
+    );
+    const kwh =
+      combineDWord(
+        dataKwh.response.body.values[0],
+        dataKwh.response.body.values[1]
+      ) / 1000;
+
     const form = new FormData();
     form.append("area", areas[slaveId]);
-    form.append("kwh", dataKwh.response.body.values[1]);
-    form.append("a1", dataA1.response.body.values[1] / 1000);
-    form.append("a2", dataA2.response.body.values[1] / 1000);
-    form.append("a3", dataA3.response.body.values[1] / 1000);
-    form.append("v1", dataV1.response.body.values[1] / 10);
-    form.append("v2", dataV2.response.body.values[1] / 10);
-    form.append("v3", dataV3.response.body.values[1] / 10);
+    form.append("kwh", kwh);
+    form.append("a1", a1);
+    form.append("a2", a2);
+    form.append("a3", a3);
+    form.append("v1", v1);
+    form.append("v2", v2);
+    form.append("v3", v3);
+    form.append("cosP", cosP);
+    form.append("thdA1", thdA1);
+    form.append("thdA2", thdA2);
+    form.append("thdA3", thdA3);
+    form.append("thdV1", thdV1);
+    form.append("thdV2", thdV2);
+    form.append("thdV3", thdV3);
 
     await axios.post(process.env.server_url, form, {
       headers: {
@@ -121,13 +248,20 @@ const getPowerMeterData = async (slaveId) => {
       },
     });
 
-    log("green", `kwh: ${dataKwh.response.body.values[1]}`);
-    log("green", `a1: ${dataA1.response.body.values[1] / 1000}`);
-    log("green", `a2: ${dataA2.response.body.values[1] / 1000}`);
-    log("green", `a3: ${dataA3.response.body.values[1] / 1000}`);
-    log("green", `v1: ${dataV1.response.body.values[1] / 10}`);
-    log("green", `v2: ${dataV2.response.body.values[1] / 10}`);
-    log("green", `v3: ${dataV3.response.body.values[1] / 10}`);
+    log("green", `kwh: ${kwh}`);
+    log("green", `a1: ${a1}`);
+    log("green", `a2: ${a2}`);
+    log("green", `a3: ${a3}`);
+    log("green", `v1: ${v1}`);
+    log("green", `v2: ${v2}`);
+    log("green", `v3: ${v3}`);
+    log("green", `cosP: ${cosP}`);
+    log("green", `THD A1: ${thdA1}`);
+    log("green", `THD A2: ${thdA2}`);
+    log("green", `THD A3: ${thdA3}`);
+    log("green", `THD V1: ${thdV1}`);
+    log("green", `THD V2: ${thdV2}`);
+    log("green", `THD V3: ${thdV3}`);
   } catch (error) {
     console.log(error);
   }
